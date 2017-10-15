508b6c3ff9af7f636559df65e3ef96f168b4ef93
circlechu <circlechu@gmail.com>
2017-10-14 21:58:14 -0400

    Add Web Api Solution

---
+++ src/iRptApi/iRpt.DataBase/Tables/Client.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Client]
+(
+	[ClientId] BIGINT NOT NULL , 
+    [ClientCode] NVARCHAR(30) NOT NULL, 
+    [ClientName] NVARCHAR(200) NOT NULL, 
+    [ts] ROWVERSION NOT NULL, 
+    CONSTRAINT [PK_Client] PRIMARY KEY ([ClientId])
+)
---
+++ src/iRptApi/iRpt.DataBase/Tables/Fund.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Fund]
+(
+	[FundId] BIGINT NOT NULL , 
+    [FundCode] NVARCHAR(50) NOT NULL, 
+    [FundName] NVARCHAR(200) NOT NULL, 
+	[ClientId] BIGINT NOT NULL, 
+    [ts] ROWVERSION NOT NULL, 
+    CONSTRAINT [PK_Fund] PRIMARY KEY ([FundId]), 
+    CONSTRAINT [FK_Fund_Client] FOREIGN KEY (CLientId) REFERENCES Client(ClientId) 
+)
