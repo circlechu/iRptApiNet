d65e4e06305e744cc4faf8533016072e4279d6e4
circlechu <circlechu@gmail.com>
2017-10-22 18:39:05 -0400

    add first sample

---
+++ src/iRptApi/iRpt.Web.Api.Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iRpt.Web.Api.Models
+{
+    public class Client
+    {
+        public Client() { }
+        public long Clientid { get; set; }
+        public string Clientcode { get; set; }
+        public string Clientname { get; set; }
+    }
+}
---
+++ src/iRptApi/iRpt.Web.Api/Controllers/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using iRpt.Web.Api.Models;
+
+namespace iRpt.Web.Api.Controllers
+{
+    public class ClientController : ApiController
+    {
+//        [Route("{id:long}", Name = "GetClientRoute")]
+        public Client GetClient(long id)
+        {
+            return  new Client {Clientid= id,Clientcode = "MATA",Clientname = "Miracle Capital"};
+        }
+    }
+
+    
+
+}
