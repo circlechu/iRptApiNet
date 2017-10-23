5dd19e9769598e0081d2a6125f78d7ef62a7c90e
circlechu <circlechu@gmail.com>
2017-10-22 22:22:25 -0400

    add put and delete

--- src/iRptApi/iRpt.Web.Api/Controllers/ClientController.cs
+++ src/iRptApi/iRpt.Web.Api/Controllers/ClientController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using iRpt.Common.Extensions;
 using iRpt.Web.Api.Models;
+using Newtonsoft.Json;
 
 namespace iRpt.Web.Api.Controllers
 {
@@ -20,9 +21,40 @@ namespace iRpt.Web.Api.Controllers
         [HttpPost]
         public IHttpActionResult AddClient(HttpRequestMessage requestMessage, Client newClient)
         {
+
+            /*
+            POST http://localhost:36553/api/client HTTP/1.1
+            Content-Type: text/json
+
+            {"ClientCode":"MATA","ClientName":"Miracle Capital"}
+
+             */
             newClient.Clientid = 2;
             var result = new CreationActionResult<Client>(requestMessage, newClient);
             return result;
+        }
+
+
+
+        [HttpPut]
+        public Client UpdateClient(long id, [FromBody] object updatedClient)
+        {
+            /*
+              PUT http://localhost:36553/api/client/1 HTTP/1.1
+              Content - type: application / json
+
+              { "ClientCode":"MATA","ClientName":"Miracle Capital"}
+            */
+
+            var client = new Client {Clientid = id, Clientname = "Client 1", Clientcode = "client1"};
+            return client;
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteClient(long id)
+        {
+            
+            return Ok();
         }
     }
 
