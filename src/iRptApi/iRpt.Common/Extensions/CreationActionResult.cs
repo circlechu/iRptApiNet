d22b4f9cee9c574a5fbf0acae3b85e6bbba9218a
circlechu <circlechu@gmail.com>
2017-10-22 20:24:06 -0400

    add creation api action

---
+++ src/iRptApi/iRpt.Common/Extensions/CreationActionResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace iRpt.Common.Extensions
+{
+    public class CreationActionResult<T> : IHttpActionResult
+    {
+        private readonly T _created;
+        private readonly HttpRequestMessage _requestMessage;
+        public CreationActionResult(HttpRequestMessage requestMessage,
+            T created)
+        {
+            _requestMessage = requestMessage;
+            _created = created;
+        }
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return System.Threading.Tasks.Task.FromResult(Execute());
+        }
+
+        public HttpResponseMessage Execute()
+        {
+            var responseMessage = _requestMessage.CreateResponse(
+                HttpStatusCode.Created, _created);
+
+            return responseMessage;
+        }
+    }
+}
--- src/iRptApi/iRpt.Web.Api/Controllers/ClientController.cs
+++ src/iRptApi/iRpt.Web.Api/Controllers/ClientController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using iRpt.Common.Extensions;
 using iRpt.Web.Api.Models;
 
 namespace iRpt.Web.Api.Controllers
@@ -14,6 +15,14 @@ namespace iRpt.Web.Api.Controllers
         public Client GetClient(long id)
         {
             return  new Client {Clientid= id,Clientcode = "MATA",Clientname = "Miracle Capital"};
+        }
+
+        [HttpPost]
+        public IHttpActionResult AddClient(HttpRequestMessage requestMessage, Client newClient)
+        {
+            newClient.Clientid = 2;
+            var result = new CreationActionResult<Client>(requestMessage, newClient);
+            return result;
         }
     }
 
