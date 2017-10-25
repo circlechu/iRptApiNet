6488d970256e3b16346f4cde73ee0dac003c46df
circlechu <circlechu@gmail.com>
2017-10-24 22:34:04 -0400

    add log in global.aspx.cs

--- src/iRptApi/iRpt.Web.Api/Global.asax.cs
+++ src/iRptApi/iRpt.Web.Api/Global.asax.cs
@@ -1,20 +1,29 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Http;
 using iRpt.Common.Logging;
 using iRpt.Common.Security;
 using iRpt.Common.TypeMapping;
 using iRpt.Web.Common;
+using log4net;
 
 namespace iRpt.Web.Api
 {
     public class WebApiApplication : HttpApplication
     {
+        private static readonly ILog _log = WebContainerManager.Get<ILogManager>().GetLog(typeof(WebApiApplication));
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RegisterHandlers();
-//            new AutoMapperConfigurator().Configure(
-//                WebContainerManager.GetAll<IAutoMapperTypeConfigurator>());
+            try
+            {
+                new AutoMapperConfigurator().Configure(WebContainerManager.GetAll<IAutoMapperTypeConfigurator>());
+            }
+            catch (Exception e)
+            {
+                _log.ErrorFormat("Application_Start:{0}",e.Message);
+            }
         }
 
         private void RegisterHandlers()
@@ -22,5 +31,16 @@ namespace iRpt.Web.Api
             var logManager = WebContainerManager.Get<ILogManager>();
             var userSession = WebContainerManager.Get<IUserSession>();
         }
+
+        protected void Application_Error()
+        {
+            var exception = Server.GetLastError();
+
+            if (exception != null)
+            {
+                _log.Error("Unhandled exception.", exception);
+            }
+        }
+
     }
 }
